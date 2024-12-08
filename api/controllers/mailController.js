3fc535359b181eacb8f3134ba53ee50d69e3c499
Deval Kasundra <dkdeval13@gmail.com>
2024-12-08 11:52:32 +0530

    nodemailer and mongoose upgraded

--- api/controllers/mailController.js
+++ api/controllers/mailController.js
@@ -2,30 +2,29 @@
 
 const nodeMailer = require('nodemailer');
 
-exports.sendMail = function(req,res){
-  const transporter = nodeMailer.createTransport({
-    host: 'smtp.zoho.com',
-    port: 465,
-    secure: true,  //true for 465 port, false for other ports
-    auth: {
-      user: 'email@example.com',
-      pass: 'password'
-    }
-  });
+exports.sendMail = async function (req, res) {
+  try {
+    const transporter = nodeMailer.createTransport({
+      host: 'smtp.zoho.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: 'email@example.com',
+        pass: 'password'
+      }
+    });
 
-  const mailOptions = {
-    from: '"Your Name" <youremail@example.com>', // sender address
-    to: 'abc@example.com, xyz@example.com', // list of receivers
-    subject: 'Hello ', // Subject line
-    text: 'Hello world?', // plain text body
-    html: '<b>Hello world?</b>' // html body
-  };
+    const mailOptions = {
+      from: '"Your Name" <youremail@example.com>', // sender address
+      to: 'abc@example.com, xyz@example.com', // list of receivers
+      subject: 'Hello ', // Subject line
+      text: 'Hello world?', // plain text body
+      html: '<b>Hello world?</b>' // html body
+    };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      res.status(400).send(error);
-    } else {
-      res.status(200).send(info);
-    }
-  });
-}
+    const info = await transporter.sendMail(mailOptions);
+    res.status(200).send(info);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
